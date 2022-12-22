49d8144eb96429246066e88b394c693be975b954
denisson <denisson.graca3@gmail.com>
2022-12-22 16:07:26 -0300

    slide ok

--- slide-stories.js
+++ slide-stories.js
@@ -1,10 +1,11 @@
 class SlideStories {
+  //1
   constructor(id) {
     this.slide = document.querySelector(`[data-slide="${id}"]`);
     this.active = 0;
     this.init();
   }
-
+  //4
   activeSlide(index) {
     this.active = index;
     this.items.forEach((item) => item.classList.remove('active'));
@@ -13,7 +14,7 @@ class SlideStories {
     this.thumbItems[index].classList.add('active');
     this.autoSlide();
   }
-
+  //6.2.1
   prev() {
     if (this.active > 0) {
       this.activeSlide(this.active - 1);
@@ -21,7 +22,7 @@ class SlideStories {
       this.activeSlide(this.items.length - 1);
     }
   }
-
+  //6.2
   next() {
     if (this.active < this.items.length - 1) {
       this.activeSlide(this.active + 1);
@@ -29,24 +30,24 @@ class SlideStories {
       this.activeSlide(0);
     }
   }
-
+  //6.1
   addNavigation() {
     const nextBtn = this.slide.querySelector('.slide-next');
     const prevBtn = this.slide.querySelector('.slide-prev');
     nextBtn.addEventListener('click', this.next);
     prevBtn.addEventListener('click', this.prev);
   }
-
+  //3
   addThumbItems() {
     this.items.forEach(() => (this.thumb.innerHTML += `<span></span>`));
-    this.thumbItems = Array.from(this.thumb.children);
+    this.thumbItems = Array.from(this.thumb.children); //sendo criado aqui
   }
-
+  //5
   autoSlide() {
-    clearTimeout(this.timeout);
-    this.timeout = setTimeout(this.next, 5000);
+    clearTimeout(this.timeout); // ja limpa
+    this.timeout = setTimeout(this.next, 5000); //depois usa
   }
-
+  //2
   init() {
     this.next = this.next.bind(this);
     this.prev = this.prev.bind(this);
